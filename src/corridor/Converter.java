5eae4def8f3760721438d52d0bc47379ad3d19e9
Gennadiy Koganovich <genakoganovich@gmail.com>
2019-06-05 18:02:36 +0300

    0032: adds left and right corridor width

--- src/corridor/Converter.java
+++ src/corridor/Converter.java
@@ -1,10 +1,13 @@
 package corridor;
-
 import java.util.Vector;
 
 abstract class Converter {
     public static final int NOT_FOR_USE = -999;
-    public static final int INITIAL_CORRIDOR_WIDTH = 1000;
+    public static final int CORRIDOR_WIDTH_LEFT = 500;
+    public static final int CORRIDOR_WIDTH_RIGHT = 500;
+    public static final int INITIAL_CORRIDOR_WIDTH = (CORRIDOR_WIDTH_LEFT + CORRIDOR_WIDTH_RIGHT) / 2;
+    public static final int MINIMAL_VELOCITY = 1500;
+    public static final int REPLACEMENT_VELOCITY = 2200;
     abstract Vector<String> convertData(Vector<String> vector);
 }
 class CMPStackToZomfConverter extends Converter {
@@ -13,11 +16,17 @@ class CMPStackToZomfConverter extends Converter {
         Vector<String> res = new Vector<>();
         res.add(vector.get(0));
         res.add(vector.get(1));
-        res.add(vector.get(2));
+
+        double oldVelocity = Double.parseDouble(vector.get(2));
+        double newVelocity = ((oldVelocity - CORRIDOR_WIDTH_LEFT) + (oldVelocity + CORRIDOR_WIDTH_RIGHT)) / 2;
+        if (newVelocity - CORRIDOR_WIDTH_LEFT < MINIMAL_VELOCITY) {
+            newVelocity = MINIMAL_VELOCITY + CORRIDOR_WIDTH_LEFT + 100;
+        }
+        res.add(String.valueOf(newVelocity));
         res.add(String.valueOf(0.0));
         res.add(vector.get(3));
         res.add(String.valueOf(INITIAL_CORRIDOR_WIDTH));
-        res.add(String.valueOf(1.5));
+        res.add(String.valueOf(1.55334));
         return res;
     }
 }
