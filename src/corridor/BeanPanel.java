67a9e79ae075efb08d74028dc8c1f9cf8969c749
Gena Koganovich <genakoganovich@gmail.com>
2019-06-09 22:45:12 +0300

    0033: adds Module

--- src/corridor/BeanPanel.java
+++ src/corridor/BeanPanel.java
@@ -2,6 +2,8 @@ package corridor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class BeanPanel extends JPanel {
     private Module selectedModule = null;
@@ -12,10 +14,10 @@ public class BeanPanel extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         modulesPanel = new JPanel();
         modulesPanel.setLayout(new BoxLayout(modulesPanel, BoxLayout.Y_AXIS));
-        modulesPanel.add(new Module("Flow", this));
-        modulesPanel.add(new Module("Read", this));
-        modulesPanel.add(new Module("Convert", this));
-        modulesPanel.add(new Module("Save", this));
+        modulesPanel.add(new Module("Flow", this, new ModuleMouseListener()));
+        modulesPanel.add(new Module("Read", this, new ModuleMouseListener()));
+        modulesPanel.add(new Module("Convert", this, new ModuleMouseListener()));
+        modulesPanel.add(new Module("Save", this, new ModuleMouseListener()));
         parametersPanel = new JPanel();
         setBorder(BorderFactory.createLineBorder(Color.black));
         JSplitPane splitPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, modulesPanel, parametersPanel);
@@ -26,5 +28,40 @@ public class BeanPanel extends JPanel {
     }
     public Module getSelectedModule() {
         return selectedModule;
+    }
+
+    private class ModuleMouseListener implements MouseListener {
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            if (e.getClickCount() == 2) {
+                System.out.println("double clicked");
+            } else {
+                System.out.println("clicked");
+            }
+            Module selected = getSelectedModule();
+            if (selected != null) {
+                selected.toggleBold();
+            }
+            setSelectedModule((Module) e.getSource());
+            selected.toggleBold();
+        }
+        @Override
+        public void mousePressed(MouseEvent e) {
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+
+        }
     }
 }
--- src/corridor/Module.java
+++ src/corridor/Module.java
@@ -8,47 +8,14 @@ import java.awt.event.MouseListener;
 
 public class Module extends JLabel {
     private BeanPanel owner;
-    public Module(String text, BeanPanel owner) {
+    public Module(String text, BeanPanel owner, MouseListener mouseListener) {
         super(text);
-        addMouseListener(new ModuleMouseListener());
+        addMouseListener(mouseListener);
         this.owner = owner;
     }
-    private void toggleBold() {
+    public void toggleBold() {
         Font f = getFont();
         setFont(f.deriveFont(f.getStyle() ^ Font.BOLD));
     }
-    private class ModuleMouseListener implements MouseListener {
-        @Override
-        public void mouseClicked(MouseEvent e) {
-            if (e.getClickCount() == 2) {
-                System.out.println("double clicked");
-            } else {
-                System.out.println("clicked");
-            }
-            Module selected = owner.getSelectedModule();
-            if (selected != null) {
-                selected.toggleBold();
-            }
-            owner.setSelectedModule(Module.this);
-            toggleBold();
-        }
-        @Override
-        public void mousePressed(MouseEvent e) {
-        }
 
-        @Override
-        public void mouseReleased(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseEntered(MouseEvent e) {
-
-        }
-
-        @Override
-        public void mouseExited(MouseEvent e) {
-
-        }
-    }
 }
